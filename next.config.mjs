bcf45f2e0a09db90dc892d703e0c10bd83458704
Dawid Smoleń <smolendawid@gmail.com>
2024-04-05 16:43:00 +0200

    fix: build typesript build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import { i18n } from './next-i18next.config.mjs'
+// import { i18n } from './next-i18next.config.mjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
+  // i18n,
 }
 
 export default nextConfig
